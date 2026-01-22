e56a55e2254bc36ce760d8eda3d3ac0ed30a0cec
ziyadalawami-hash <ziyadalawami@gmail.com>
2026-01-23 00:23:03 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Content from './components/Content';
 import MarqueeBar from './components/MarqueeBar';
+import { useLanguage } from './i18n/LanguageContext';
 
 function App() {
+  const { isRTL } = useLanguage();
+
+  useEffect(() => {
+    document.documentElement.setAttribute('dir', isRTL ? 'rtl' : 'ltr');
+  }, [isRTL]);
+
   return (
     <div id="top" className="bg-[#000a01] min-h-screen md:pt-10 pb-14 md:pb-0">
       <MarqueeBar />
