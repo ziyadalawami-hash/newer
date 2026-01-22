0d1ca5f6ca62e007af06acded69f34418b5f328d
ziyadalawami-hash <ziyadalawami@gmail.com>
2026-01-23 00:11:44 +0200

    Updated index.css

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -179,7 +179,7 @@ const MainContent = () => {
                         </button>
                       </div>
 
-                      <div className="flex-1 flex flex-col p-6 pt-4 pb-20 min-h-0 overflow-y-auto">
+                      <div className="flex-1 flex flex-col p-6 pt-4 pb-20 min-h-0 overflow-y-auto project-card-scroll">
                         <div className="mb-3">
                           <h2 className="text-lg font-bold text-gray-800 mb-3">
                             {project.title}
