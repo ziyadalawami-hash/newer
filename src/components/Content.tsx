023e3349808f2e37fad3b94f6eb40440603d2194
ziyadalawami-hash <ziyadalawami@gmail.com>
2026-01-22 23:05:29 +0200

    Updated Content.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -163,7 +163,6 @@ const MainContent = () => {
                               </ul>
                             </div>
                           </div>
-                          <div className="absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-white to-transparent pointer-events-none"></div>
                         </div>
                       </div>
 
